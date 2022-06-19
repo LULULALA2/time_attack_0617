ff60de6f7c2f6f9d463152097de132d88c702151
LULULALA2 <lhg9714@gmail.com>
2022-06-19 18:54:05 +0900

    새브랜치

--- user/models.py
+++ user/models.py
@@ -2,21 +2,22 @@ from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 from django.db import models
 
 
+# custom user model 사용 시 UserManager 클래스와 create_user, create_superuser 함수가 정의되어 있어야 함
 class UserManager(BaseUserManager):
-    def create_user(self, email, password=None):
-        if not email:
+    def create_user(self, username, password=None):
+        if not username:
             raise ValueError('Users must have an username')
         user = self.model(
-            email=email,
+            username=username,
         )
         user.set_password(password)
         user.save(using=self._db)
         return user
-    
+
     # python manage.py createsuperuser 사용 시 해당 함수가 사용됨
-    def create_superuser(self, email, password=None):
+    def create_superuser(self, username, password=None):
         user = self.create_user(
-            email=email,
+            username=username,
             password=password
         )
         user.is_admin = True
@@ -25,31 +26,28 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser):
+    username = models.CharField("사용자 계정", max_length=20)
+    email = models.EmailField("이메일 주소", max_length=100, unique=True)
+    password = models.CharField("비밀번호", max_length=128)
+    fullname = models.CharField("이름", max_length=20)
+    usertype = models.ForeignKey('UserType', on_delete=models.SET_NULL())
+    join_date = models.DateTimeField("가입일", auto_now_add=True)
 
-    class Meta:
-        db_table = "user"
-
-    name = models.CharField(max_length=50, null=False)
-    username = models.CharField(max_length=20, null=False)
-    password = models.CharField(max_length=256, null=False)
-    email = models.EmailField(max_length=50, null=False, unique=True)
-    join_date = models.DateField(auto_now_add=True)
-    
     # is_active가 False일 경우 계정이 비활성화됨
-    is_active = models.BooleanField(default=True) 
+    is_active = models.BooleanField(default=True)
 
     # is_staff에서 해당 값 사용
     is_admin = models.BooleanField(default=False)
-    
+
     # id로 사용 할 필드 지정.
     # 로그인 시 USERNAME_FIELD에 설정 된 필드와 password가 사용된다.
     USERNAME_FIELD = 'email'
 
     # user를 생성할 때 입력받은 필드 지정
     REQUIRED_FIELDS = []
-    
-    objects = UserManager() # custom user 생성 시 필요
-    
+
+    objects = UserManager()  # custom user 생성 시 필요
+
     def __str__(self):
         return self.email
 
@@ -57,23 +55,31 @@ class User(AbstractBaseUser):
     # admin일 경우 항상 True, 비활성 사용자(is_active=False)의 경우 항상 False
     def has_perm(self, perm, obj=None):
         return True
-    
+
     # 로그인 사용자의 특정 app에 접근 가능 여부를 설정, app_label에는 app 이름이 들어간다.
     # admin일 경우 항상 True, 비활성 사용자(is_active=False)의 경우 항상 False
-    def has_module_perms(self, app_label): 
+    def has_module_perms(self, app_label):
         return True
-    
+
     # admin 권한 설정
     @property
-    def is_staff(self): 
+    def is_staff(self):
         return self.is_admin
 
 
-class UserProfile(models.Model):
-    user = models.OneToOneField(to=User, verbose_name="사용자", on_delete=models.CASCADE, primary_key=True)
-    birth = models.DateField()
-    bio = models.TextField(max_length=500, blank=True)
+class UserType(models.Model):
+    class Meta:
+        db_table = "usertype"
+
+    usertype = models.CharField(max_length=20)
 
     def __str__(self):
-        return f'{self.user.username}님의 프로필'
+        return self.usertype
 
+
+class UserLog(models.Model):
+    class Meta:
+        db_table = "UserLog"
+
+    login_date = models.DateField(auto_add=True)
+    last_candidate_date = models.DateField(auto_add=True)
--- user/views.py
+++ user/views.py
@@ -1,18 +1,30 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, permissions, request
 from django.contrib.auth import login, authenticate, logout
 from django.views.decorators.csrf import csrf_exempt
+from .models import User, UserType, UserLog
+
+
+class SignUpView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    usertype = request.data.get('usertype', None)
+    email = request.data.get('email', None)
+    password = request.data.get('password', None)
+    usertype = UserType.objects.get(usertype=usertype)
+
+    User(email=email, password=make_password(password), usertype=usertype).save()
 
 
 class UserView(APIView):
     @csrf_exempt
     def post(self, request):
-        username = request.data.get('username', '')
+        email = request.data.get('email', '')
         password = request.data.get('password', '')
 
-        user = authenticate(request, username=username, password=password)
+        user = authenticate(request, email=email, password=password)
         if not user:
             return Response({"error": "존재하지 않는 계정이거나 패스워드가 일치하지 않습니다."}, status=status.HTTP_401_UNAUTHORIZED)
 
